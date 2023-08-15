4d96a944a51ba5ebcf6dbd84bbd36f4627eb80ce
Efrain Vacas <efrvame@gmail.com>
2023-08-15 17:13:51 -0500

    latest version

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import { tweetsData } from "./data.js"
 import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 
-/* localStorage.setItem("myTweets", JSON.stringify(tweetsData)) */
-
 let localTweets
 
 if(localStorage.getItem("myTweets")){
